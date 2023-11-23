c71b767e5aa6a8a730fb44bdcdbf192282dc1037
TirthPatel19-44 <191130107044@sal.edu.in>
2023-11-23 16:34:37 -0500

    inital commit

---
+++ src/main/scala/livechart/LiveChart.scala
@@ -0,0 +1,49 @@
+package livechart
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.*
+
+import org.scalajs.dom
+
+// import javascriptLogo from "/javascript.svg"
+@js.native @JSImport("/javascript.svg", JSImport.Default)
+val javascriptLogo: String = js.native
+
+@main
+def LiveChart(): Unit =
+  dom.document.querySelector("#app").innerHTML = s"""
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+        <img src="$javascriptLogo" class="logo vanilla" alt="JavaScript logo" />
+      </a>
+      <h1>Hello Scala.js!</h1>
+      <h1>Hello vite and Scala.js from Tirth</h1>
+      <h2> why are you?</h2>
+      <h2> who are you? </h2>
+      <h3> Why are you </h3>
+      <h3> hello ruju </h3>
+      <div class="card">
+        <button id="counter" type="button"></button>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite logo to learn more
+      </p>
+    </div>
+  """
+
+  setupCounter(dom.document.getElementById("counter"))
+end LiveChart
+
+def setupCounter(element: dom.Element): Unit =
+  var counter = 0
+
+  def setCounter(count: Int): Unit =
+    counter = count
+    element.innerHTML = s"count is $counter"
+
+  element.addEventListener("click", e => setCounter(counter + 1))
+  setCounter(0)
+end setupCounter
